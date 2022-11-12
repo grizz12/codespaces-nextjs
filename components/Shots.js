19acd1499ec1f764e84b3a250144223f5d5e6f21
FujaTrust <83684256+grizz12@users.noreply.github.com>
2022-11-12 03:14:05 +0000

    :outbox_tray: add shots components

---
+++ components/Shots.js
@@ -0,0 +1,11 @@
+export default () => {
+  return (
+    <a
+      href="#"
+      className="flex py-2 item-center hover:text-blue-500 text-gray-600"
+    >
+      <box-icon name="camera" className="mr-2" color="currentColor"></box-icon>
+      Shots
+    </a>
+  );
+};
