4b8a365d1ec477920b46c13529e5868702f8ba92
FujaTrust <83684256+grizz12@users.noreply.github.com>
2022-11-12 03:13:48 +0000

    :outbox_tray: add setting components

---
+++ components/Settings.js
@@ -0,0 +1,11 @@
+export default () => {
+  return (
+    <a
+      href="#"
+      className="flex py-2 item-center font-medium hover:text-blue-500 text-gray-600"
+    >
+      <box-icon name="cog" className="mr-2" color="currentColor"></box-icon>
+      Settings
+    </a>
+  );
+};
